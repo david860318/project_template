7e6880aa69299a923f0a64d337774ede6732772e
ScottJian <0980258@gmail.com>
2024-07-21 21:03:10 +0800

    first-version

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import '@/styles/globals.css'
+import '@/styles/sidebar.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function MyApp({ Component, pageProps }) {
+  // 使用自訂在頁面層級的版面(layout)
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
+}
---
+++ pages/sidebar-test.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react'
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from '@ant-design/icons'
+import { Menu, ColorPicker } from 'antd'
+
+const items = [
+  {
+    key: '1',
+    icon: <MailOutlined />,
+    label: '帳篷',
+    children: [
+      {
+        key: '11',
+        label: '單/雙人',
+      },
+      {
+        key: '12',
+        label: '家庭',
+      },
+      {
+        key: '13',
+        label: '寵物',
+      },
+      {
+        key: '14',
+        label: '客廳帳/天幕',
+      },
+    ],
+  },
+  {
+    key: '2',
+    icon: <AppstoreOutlined />,
+    label: '戶外家具',
+    children: [
+      {
+        key: '21',
+        label: '露營椅',
+      },
+      {
+        key: '22',
+        label: '露營桌',
+      },
+    ],
+  },
+  {
+    key: '3',
+    icon: <SettingOutlined />,
+    label: '照明&生火',
+    children: [
+      {
+        key: '31',
+        label: '照明燈',
+      },
+      {
+        key: '32',
+        label: '木炭',
+      },
+    ],
+  },
+]
+const getLevelKeys = (items1) => {
+  const key = {}
+  const func = (items2, level = 1) => {
+    items2.forEach((item) => {
+      if (item.key) {
+        key[item.key] = level
+      }
+      if (item.children) {
+        func(item.children, level + 1)
+      }
+    })
+  }
+  func(items1)
+  return key
+}
+const levelKeys = getLevelKeys(items)
+
+export default function Sidebar() {
+  const [stateOpenKeys, setStateOpenKeys] = useState(['2', '23'])
+  const [primary, setPrimary] = React.useState('#1677ff')
+  const onOpenChange = (openKeys) => {
+    const currentOpenKey = openKeys.find(
+      (key) => stateOpenKeys.indexOf(key) === -1
+    )
+    // open
+    if (currentOpenKey !== undefined) {
+      const repeatIndex = openKeys
+        .filter((key) => key !== currentOpenKey)
+        .findIndex((key) => levelKeys[key] === levelKeys[currentOpenKey])
+      setStateOpenKeys(
+        openKeys
+          // remove repeat key
+          .filter((_, index) => index !== repeatIndex)
+          // remove current level all child
+          .filter((key) => levelKeys[key] <= levelKeys[currentOpenKey])
+      )
+    } else {
+      // close
+      setStateOpenKeys(openKeys)
+    }
+  }
+  return (
+    <>
+      <ColorPicker
+        showText
+        value={primary}
+        onChangeComplete={(color) => setPrimary(color.toHexString())}
+      />
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={['231']}
+        openKeys={stateOpenKeys}
+        onOpenChange={onOpenChange}
+        style={{
+          width: 186,
+          color: '#413c1c',
+        }}
+        
+        items={items}
+      />
+    </>
+  )
+}
